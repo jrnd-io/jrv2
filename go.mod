a812b49132a787ecb34e5eac1fc86a7c79670601
Vincenzo <Marchese>
2024-08-30 16:10:11 +0200

    feat(plugin): add console and lua plugin

--- go.mod
+++ go.mod
@@ -12,26 +12,53 @@ require (
 	github.com/spf13/cobra v1.8.1
 	github.com/squeeze69/generacodicefiscale v1.0.5
 	github.com/stretchr/testify v1.9.0
+	github.com/vadv/gopher-lua-libs v0.5.0
+	github.com/yuin/gopher-lua v1.1.1
 	golang.org/x/text v0.15.0
 	google.golang.org/grpc v1.65.0
 	google.golang.org/protobuf v1.34.2
 	gopkg.in/yaml.v3 v3.0.1
+	layeh.com/gopher-luar v1.0.11
 )
 
 require (
+	github.com/VividCortex/ewma v1.1.1 // indirect
+	github.com/alessio/shellescape v1.4.1 // indirect
+	github.com/aws/aws-sdk-go v1.34.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cbroglie/mustache v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/cheggaaa/pb/v3 v3.0.5 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/hashicorp/yamux v0.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jmespath/go-jmespath v0.3.0 // indirect
+	github.com/lib/pq v1.7.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.7 // indirect
+	github.com/mattn/go-sqlite3 v1.14.3 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/mitchellh/go-testing-interface v0.0.0-20171004221916-a61a99592b77 // indirect
+	github.com/mitchellh/mapstructure v1.3.2 // indirect
+	github.com/montanaflynn/stats v0.6.3 // indirect
 	github.com/oklog/run v1.0.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.11.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.26.0 // indirect
+	github.com/prometheus/procfs v0.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/squeeze69/codicefiscale v1.0.2 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	github.com/yuin/gluamapper v0.0.0-20150323120927-d836955830e7 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.22.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240528184218-531527333157 // indirect
+	gopkg.in/xmlpath.v2 v2.0.0-20150820204837-860cbeca3ebc // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
---
+++ pkg/plugin/luascript/plugin.go
@@ -0,0 +1,122 @@
+// Copyright © 2024 JR team
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+package luascript
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"strings"
+
+	"github.com/jrnd-io/jrv2/pkg/jrpc"
+	"github.com/jrnd-io/jrv2/pkg/plugin"
+	"github.com/rs/zerolog/log"
+	libs "github.com/vadv/gopher-lua-libs"
+	lua "github.com/yuin/gopher-lua"
+	"github.com/yuin/gopher-lua/parse"
+	luar "layeh.com/gopher-luar"
+)
+
+const (
+	ScriptName = "luajr"
+	PluginName = "luascript"
+)
+
+func init() {
+	p, err := New()
+	if err == nil {
+		plugin.RegisterLocalPlugin(PluginName, p)
+	}
+}
+
+type Plugin struct {
+	configuration    Config
+	luaProtoFunction *lua.FunctionProto
+}
+
+func New() (*Plugin, error) {
+
+	cfgBytes, err := plugin.ReadConfig(PluginName)
+	if err != nil {
+		return nil, err
+	}
+
+	config := Config{}
+	if err := json.Unmarshal(cfgBytes, &config); err != nil {
+		log.Fatal().Err(err).Msg("Failed to unmarshal config")
+	}
+
+	p := &Plugin{
+		configuration: config,
+	}
+	p.InitializeFromConfig(config)
+	return p, nil
+}
+
+func (p *Plugin) InitializeFromConfig(config Config) {
+
+	var err error
+	if config.Script == "" && config.ScriptFile == "" {
+		log.Fatal().Msg("script or script_file is required")
+	}
+
+	var scriptBytes []byte
+	if config.ScriptFile != "" {
+		scriptBytes, err = os.ReadFile(config.ScriptFile)
+		if err != nil {
+			log.Fatal().Err(err).Str("script_file", config.ScriptFile).Msg("Failed to read script file")
+		}
+	} else {
+		scriptBytes = []byte(config.Script)
+	}
+
+	chunk, err := parse.Parse(strings.NewReader(string(scriptBytes)), ScriptName)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to parse script")
+	}
+	p.luaProtoFunction, err = lua.Compile(chunk, ScriptName)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to compile script")
+	}
+
+	p.configuration = config
+
+}
+
+func (p *Plugin) Produce(_ context.Context, k []byte, v []byte, h map[string]string) (*jrpc.ProduceResponse, error) {
+
+	L := lua.NewState()
+	libs.Preload(L)
+
+	L.SetGlobal("k", lua.LString(k))
+	L.SetGlobal("v", lua.LString(string(v)))
+	L.SetGlobal("headers", luar.New(L, h))
+
+	lf := L.NewFunctionFromProto(p.luaProtoFunction)
+	L.Push(lf)
+	err := L.PCall(0, 0, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &jrpc.ProduceResponse{
+		Bytes:   uint64(len(v)),
+		Message: "",
+	}, nil
+
+}
