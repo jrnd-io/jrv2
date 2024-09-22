7897063865a8a7aaf563a7f684ebbfad996695ed
ugol <ugo.landini@gmail.com>
2024-09-22 17:18:25 +0200

    added throughput calculation to run command

--- cmd/jr/template/run.go
+++ cmd/jr/template/run.go
@@ -87,7 +87,7 @@ func run(cmd *cobra.Command, args []string) {
 		if err != nil {
 			panic(err)
 		}
-		frequency = api.CalculateFrequency(bytes, num, throughput)
+		frequency = api.CalculateFrequency(len(bytes), num, throughput)
 	}
 
 	e, err := api.NewEmitter(
